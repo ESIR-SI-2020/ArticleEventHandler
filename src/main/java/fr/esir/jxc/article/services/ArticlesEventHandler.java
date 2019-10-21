6e76d44c77e516b92af470ff8e89d36fcdd59b41
Antoine Cheron <cheron.antoine@gmail.com>
2019-10-21 21:08:23 +0200

    Add basic kafka consumer and article event router

--- src/main/java/fr/esir/jxc/article/services/ArticlesEventHandler.java
+++ src/main/java/fr/esir/jxc/article/services/ArticlesEventHandler.java
@@ -1,4 +1,29 @@
 package fr.esir.jxc.article.services;
 
+import org.springframework.stereotype.Service;
+
+import fr.esir.jxc.article.events.ArticleAdded;
+import fr.esir.jxc.article.events.ArticleDeleted;
+import fr.esir.jxc.article.events.ArticleShared;
+import fr.esir.jxc.article.events.ArticleTagsSet;
+
+@Service
 public class ArticlesEventHandler {
+
+  public void handleArticleAddedEvent(ArticleAdded articleAdded) {
+    // TODO
+  }
+
+  public void handleArticleDeletedEvent(ArticleDeleted articleDeleted) {
+    // TODO
+  }
+
+  public void handleArticleSharedEvent(ArticleShared articleShared) {
+    // TODO
+  }
+
+  public void handleArticleTagSetEvent(ArticleTagsSet articleTagsSet) {
+    // TODO
+  }
+
 }
